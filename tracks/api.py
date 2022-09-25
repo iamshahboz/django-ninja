006686bcc5590e240353432eb40d771d7a71985a
Shahboz <=>
2022-09-25 15:18:35 +0600

    api for put, post, delete created
    put method will allow to make changes on existing tracks
    post will create new track and delete will delete them

--- tracks/api.py
+++ tracks/api.py
@@ -23,6 +23,36 @@ def track(request, track_id:int):
     except Track.DoesNotExist as e:
         return 404, {'message':'track does not exist'}
 
+@api.post("/tracks", response={201: TrackSchema})
+def create_track(request, track:TrackSchema):
+    track = Track.objects.create(**track.dict())
+    return track  
+
+@api.put('/tracks/{track_id}',response={200:TrackSchema,404:NotFoundSchema})
+def change_track(request,track_id:int,data:TrackSchema):
+    try:
+        track = Track.objects.get(pk=track_id)
+        for attribute,value in data.dict().items():
+            setattr(track,attribute,value)
+        track.save()
+        return 200, track
+    except Track.DoesNotExist as e:
+        return 404, {'message':'track does not exist'}
+
+@api.delete('/tracks/{track_id}',response={200:None,404:NotFoundSchema})
+def delete_track(request,track_id:int,data:TrackSchema):
+    try:
+        track = Track.objects.get(pk=track_id)
+        track.delete()
+        return 200
+    except Track.DoesNotExist as e:
+        return 404, {'message':'track does not exist'}
+
+
+
+
+
+
 
 
 
